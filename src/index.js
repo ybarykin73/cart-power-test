2547c721c36f4d9be756227a0c1f9f1d6465b77a
YuraBarykin <yBarykin@gmail.com>
2023-11-11 13:04:36 +0400

    Верстка блока продуктов

--- src/index.js
+++ src/index.js
@@ -15,11 +15,10 @@ $('.search__wrapper-mobile-open-js').click(function(){
   $(this).prev('.search__wrapper').addClass('active')
 });
 
-const swiper = new Swiper('#portfolio-slider-js', {
+const swiperPortfolio = new Swiper('#portfolio-slider-js', {
   modules: [Navigation],
   slidesPerView: 2,
   spaceBetween: 10,
-  // loop: true,
   breakpoints: {
     560: {
       slidesPerView: 3
@@ -33,8 +32,32 @@ const swiper = new Swiper('#portfolio-slider-js', {
     }
   },
   navigation: {
-    nextEl: ".portfolio-slider-next-js",
+    nextEl: '.portfolio-slider-next-js',
     prevEl: '.portfolio-slider-prev-js',
     disabledClass: 'disabled'
+  }
+});
+
+const swiperProducts = new Swiper('#products-slider-js', {
+  modules: [Navigation],
+  slidesPerView: 1,
+  loop: true,
+  breakpoints: {
+    560: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 20
+    }
+  },
+  navigation: {
+    nextEl: '.products-slider__button-next-js',
+    prevEl: '.products-slider__button-prev-js',
   }
 });
